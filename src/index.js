105cc0c91d2612bcc217b56d37885d5771fb5f06
Shanley Caswell <shanleycaswell@gmail.com>
2023-05-29 10:27:23 -0400

    task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener ('submit', (event) => {
+    event.preventDefault()
+    buildToDo(event.target["new-task-description"].value)
+    form.reset()
+  })
+
+function buildToDo(chore){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' x '
+  li.textContent = ` ${chore} `
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+ 
+function handleDelete(event){
+    event.target.parentNode.remove()
+}
